d5eab1ba6d26bc47d85e0069e151bbb9dcce8a1f
mitgobla <ben-dodd@outlook.com>
2018-08-19 17:11:29 +0100

    Support Discord Documentation field names
    This adds the ability to convert a dictionary with key names of the Discord Documentation field names into the RPC format.

--- example.py
+++ example.py
@@ -9,18 +9,33 @@ print("RPC connection successful.")
 time.sleep(5)
 start_time = time.time()
 while True:
+
+    # Supports using the format from the Discord Documentation
+    # Reference: https://discordapp.com/developers/docs/rich-presence/how-to#rich-presence-field-requirements
     activity = {
-            "state": "This is a sample state.",
-            "details": "This is a sample detail.",
-            "timestamps": {
-                "start": start_time
-            },
-            "assets": {
-                "small_text": "Text for small_image",
-                "small_image": "img_small",
-                "large_text": "Text for large_image",
-                "large_image": "img_large"
-            }
+        "state": "This is a sample state.",
+        "details": "This is a sample detail.",
+        "startTimestamp": start_time,
+        "smallImageText": "Text for small_image",
+        "smallImageKey": "img_small",
+        "largeImageText": "Text for large_image",
+        "largeImageKey": "img_large"
+    }
+
+    # Supports using RPC format
+    # Reference: https://github.com/discordapp/discord-rpc/blob/master/documentation/hard-mode.md
+    activity_rpc = {
+        "state": "This is a sample state.",
+        "details": "This is a sample detail.",
+        "timestamps": {
+            "start": start_time
+        },
+        "assets": {
+            "small_text": "Text for small_image",
+            "small_image": "img_small",
+            "large_text": "Text for large_image",
+            "large_image": "img_large"
         }
+    }
     rpc_obj.set_activity(activity)
     time.sleep(30)
